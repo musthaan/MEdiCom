cdcbbe0a43fb193e0f81e14f9294c4d1233457bc
musthaan <musthaan@gmail.com>
2018-06-27 03:06:00 +0530

    home page and menu

--- MEdiCom.UI/MainWindow.xaml.cs
+++ MEdiCom.UI/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace MEdiCom.UI
         {
             InitializeComponent();
         }
+
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            var userName = txtUserName.Text.Trim();
+            var password = txtPassword.Password.ToString();
+            if (validate(userName, password)) {
+                if (verifyUser(userName, password))
+                {
+                    wndHome wnd = new wndHome();
+                    this.Hide();
+                    wnd.Show();
+                }
+                else
+                {
+                    lblMsg.Content = "Invalid Username or Password!";
+                }
+            }
+        }
+
+        bool verifyUser(string username, string password)
+        {
+            return true;
+        }
+
+        bool validate(string username, string password)
+        {
+            var flag = true;
+            if (string.IsNullOrEmpty(username))
+            {
+                lblMsg.Content = "Enter user name";
+                return false;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                lblMsg.Content = "Enter password";
+                return false;
+            }
+            return true;
+        }
     }
 }
